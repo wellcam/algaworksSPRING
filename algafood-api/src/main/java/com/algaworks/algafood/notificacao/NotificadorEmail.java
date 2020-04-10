cc741cc6b2f895d2bca1f010ad3f6b4492893cea
Wellington Camilo <wel.camilo10@gmail.com>
2020-04-09 23:57:03 -0300

    Utilizando propriedades customizadas com @Value

--- algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -11,9 +12,15 @@ import com.algaworks.algafood.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
+	
 	@Override
 	public void notificar (Cliente cliente, String mensagem) {
-		
+		System.out.println(host);
+		System.out.println(porta);
 		System.out.printf("Notificação %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
 		
