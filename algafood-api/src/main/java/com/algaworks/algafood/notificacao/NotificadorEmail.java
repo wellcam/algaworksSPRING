a129e7362d241f63182c0e8ddd0c67533fa453c6
Wellington Camilo <wel.camilo10@gmail.com>
2020-04-10 00:13:49 -0300

    Criando configuração de proprieda customizada via classe

--- algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,26 +1,25 @@
 package com.algaworks.algafood.notificacao;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.modelo.Cliente;
+import com.algaworks.algafood.notificacao.properties.NotificadorProperties;
 
 @Profile("prod")
 @Primary
 @Component
 public class NotificadorEmail implements Notificador {
-
-	@Value("${notificador.email.host-servidor}")
-	private String host;
-	@Value("${notificador.email.porta-servidor}")
-	private Integer porta;
+	
+	@Autowired
+	private NotificadorProperties notificadorProperties;
 	
 	@Override
 	public void notificar (Cliente cliente, String mensagem) {
-		System.out.println(host);
-		System.out.println(porta);
+		System.out.println(notificadorProperties.getHostServidor());
+		System.out.println(notificadorProperties.getPortaServidor());
 		System.out.printf("Notificação %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
 		
